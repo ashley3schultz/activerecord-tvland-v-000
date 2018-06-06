80dcdc7d963269852e36190a4f0bd2bc6bd9d2ab
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-06 20:16:48 +0000

    automatically backed up by learn

--- db/migrate/011_delete_catchphrase_column_to_characters.rb
+++ db/migrate/011_delete_catchphrase_column_to_characters.rb
@@ -1,5 +1,5 @@
-class AddCatchphraseColumnToCharacters < ActiveRecord::Migration[4.2]
+class DeleteCatchphraseColumnToCharacters < ActiveRecord::Migration[4.2]
   def change
-    add_column :characters, :catchphrase, :string
+    remove_column :characters, :catchphrase
   end
 end
