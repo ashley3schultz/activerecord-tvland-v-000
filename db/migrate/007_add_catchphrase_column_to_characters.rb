71a181c3ee70c8e170d773aa69f4a11e864884ae
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-06 19:27:15 +0000

    automatically backed up by learn

--- db/migrate/007_add_catchphrase_column_to_characters.rb
+++ db/migrate/007_add_catchphrase_column_to_characters.rb
@@ -1,5 +1,5 @@
 class AddCatchphraseColumnToCharacters < ActiveRecord::Migration[4.2]
   def change
-    add_column :shows, :genre, :string
+    add_column :characters, :catchphrase, :string
   end
 end
