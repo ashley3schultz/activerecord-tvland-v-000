892c42c30775ac0466569181d19e52c593559fbd
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-06 20:28:17 +0000

    automatically backed up by learn

--- db/migrate/013_add_columns_to_characters.rb
+++ db/migrate/013_add_columns_to_characters.rb
@@ -1,4 +1,4 @@
-class AddColumnsToShows < ActiveRecord::Migration[4.2]
+class AddColumnsToCharacters < ActiveRecord::Migration[4.2]
   def change
     add_column :characters, :catchphrase, :string
   end
